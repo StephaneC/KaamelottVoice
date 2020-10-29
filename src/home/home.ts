78b662d3faada9153b885dd563f0c978f11d857e
StephaneC <Stephane.castrec@gmail.com>
2020-10-29 21:31:57 +0100

    Update home

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,10 +1,9 @@
 import { configureHandler } from "./IntentHandler";
 import { conversation } from '@assistant/conversation';
-const functions = require('firebase-functions');
 
 const app = conversation()
 
 // Create an app instance
 configureHandler(app);
 
-export const handler = functions.https.onRequest(app)
+export const handler = app;
